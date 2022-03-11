11f740e13bc0ac9aea9c49d4a4f40c59fc8df62b
TejShind <tej.gholap25@gmail.com>
2022-03-11 10:43:20 +0530

    [Tejaswini]ADDED:Feature for find the maximum of given three Integer by using

--- src/com/blz/generics/FindMaximum.java
+++ src/com/blz/generics/FindMaximum.java
@@ -4,14 +4,25 @@ public class FindMaximum {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Program");
-        int firstValue = 22;
-        int secondValue = 10;
-        int thirdValue = 54;
-        if (firstValue >= secondValue && firstValue >= thirdValue)
-            System.out.println(firstValue + " Maximum number is");
-        else if (secondValue >= firstValue && secondValue >= thirdValue)
-            System.out.println(secondValue + " Maximum number is");
-        else
-            System.out.println(thirdValue + " Maximum number is");
+        FindMaximum findMaximum = new FindMaximum();
+        System.out.println("Maximum number is : " + findMaximum.findMaxIntValue(22, 3, 6));
+    }
+        public int findMaxIntValue ( Integer firstValue, Integer secondValue, Integer thirdValue)
+        {
+            if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0) {
+                return firstValue;
+            }
+         else if (secondValue.compareTo(thirdValue) > 0 && secondValue.compareTo(firstValue) > 0) {
+            return secondValue;
+        } else {
+            return thirdValue;
+        }
     }
 }
+
+//        if (firstValue >= secondValue && firstValue >= thirdValue)
+//            System.out.println(firstValue + " Maximum number is");
+//        else if (secondValue >= firstValue && secondValue >= thirdValue)
+//            System.out.println(secondValue + " Maximum number is");
+//        else
+//            System.out.println(thirdValue + " Maximum number is");
